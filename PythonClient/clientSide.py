ee31a552ca0fa062493cb98f1ac096f26d7abf7e
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-11-14 22:40:28 -0600

    [Update]

--- PythonClient/clientSide.py
+++ PythonClient/clientSide.py
@@ -3,6 +3,6 @@ port = 6666
 flag = True
 while flag:
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect(('localhost', port))
+    clientSocket.connect(('127.0.0.1', port))
     message = input("type your message and presss enter to send...\n")
     clientSocket.send(message.encode())
