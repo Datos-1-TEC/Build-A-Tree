d7208f455a510e934d45193ab0e986aa84a65386
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-11-15 23:06:53 -0600

    [Created] Class ChallengeCreator

---
+++ JavaServer/Challenges/ChallengeGenerator.java
@@ -0,0 +1,13 @@
+package JavaServer.Challenges;
+
+public class ChallengeGenerator {
+    String[] challengeList = {"BST", "AVL", "SPLAY", "BTREE"};
+
+    //Metodo para generar los tokens
+
+    //Método para parsear esos tokens a JSON
+
+    //Método para pasear JSON a String
+
+    
+}
