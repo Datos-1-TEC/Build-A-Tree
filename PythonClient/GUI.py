849a8865b8ab00142b4e97672a3b3cf49bd21443
Maxg2212 <maxgarro22@gmail.com>
2020-11-18 23:18:54 -0600

    [Update] GUI python

--- PythonClient/GUI.py
+++ PythonClient/GUI.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-import mainGameScreen
+
 
 pygame.init()
 
@@ -11,13 +11,15 @@ y= 50
 width = 40
 height = 60
 vel = 5
+isJump = False
+jumpCount = 10
 
 window = pygame.display.set_mode((displayWidth,displayHeight))
 
 pygame.display.set_caption("Build a Tree")
 
 displayFlag = True
-while run:
+while displayFlag:
     pygame.time.delay(100)
 
     for event in pygame.event.get():
@@ -30,10 +32,23 @@ while run:
         x -= vel
     if keys[pygame.K_RIGHT] and x < 750 - width - vel:
         x += vel
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-    if keys[pygame.K_DOWN] and y < 500 - height - vel:
-        y += vel
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCOunt >= -10:
+            negative = 1
+            if jumpCount < 0:
+                negative = -1
+            y -= (jumpCount ** 2) * 0.5 * negative
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
     
 
 
