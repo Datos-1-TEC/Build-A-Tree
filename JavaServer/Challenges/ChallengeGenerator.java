f31cc3da29b899813d6d2c637991bf8bb360192d
igmochang <ig.morales@estudiantec.cr>
2020-11-20 22:49:27 -0600

    [update] Avance de challengeGenerator

--- JavaServer/Challenges/ChallengeGenerator.java
+++ JavaServer/Challenges/ChallengeGenerator.java
@@ -1,9 +1,27 @@
 package JavaServer.Challenges;
+import JavaServer.TokensPrototype.*;
+
+import java.util.Random;
 
 public class ChallengeGenerator {
-    String[] challengeList = {"BST", "AVL", "SPLAY", "BTREE"};
+    private String[] challengeList = {"BST", "AVL", "SPLAY", "BTREE"};
+    private String challenge;
+    private CloneFactory cloneFactory = new CloneFactory();
+    private Diamond bstToken = new Diamond();
+    private Circle avlToken = new Circle();
+    private Rectangle bToken = new Rectangle();
+    private Triangle splayToken = new Triangle();
+
 
     //Metodo para generar los tokens
+    public void challengeSelector() {
+        Random r =  new Random();
+        int randomN = r.nextInt(challengeList.length);
+        if (randomN == 0){
+            String challenge = challengeList[randomN];
+
+        }
+    }
 
     //Método para parsear esos tokens a JSON
 
