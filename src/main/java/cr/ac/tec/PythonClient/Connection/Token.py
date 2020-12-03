49c95c48fdfa1140c09027ed0f5c7b472e24cffa
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-12-03 00:06:47 -0600

    [Added] Token Python Object

---
+++ src/main/java/cr/ac/tec/PythonClient/Connection/Token.py
@@ -0,0 +1,15 @@
+class Token():
+    def __init__(self, val, shape):
+        super().__init__()
+        self.val = val
+        self.shape = shape
+    
+    def getVal(self):
+        return self.val
+    
+    def getShape(self):
+        return self.shape
+
+
+
+
