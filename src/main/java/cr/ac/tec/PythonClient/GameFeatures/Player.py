5b4c7e1f04046f4aa6cb9e2a7de4870547d78ff0
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-12-05 19:09:07 -0600

    [Update] Python Token class
    Se añaden metodos para pasar un objeto token a JSON y viceversa

--- src/main/java/cr/ac/tec/PythonClient/GameFeatures/Player.py
+++ src/main/java/cr/ac/tec/PythonClient/GameFeatures/Player.py
@@ -1,12 +1,12 @@
-class Player():
-    def __init__(self, ID, currentToken = null, score = 0, treeList = []):
+
+class Player(object):
+    def __init__(self, ID, currentToken = None, score = 0, treeList = []):
         super().__init__()
         self.ID = ID
         self.lives = 3
         self.currentToken = currentToken
         self.score = score
         self.treeList = treeList
-        self.currentPowerUp = currentPowerUp
     
     def getID(self):
         return self.ID
@@ -22,9 +22,6 @@ class Player():
     
     def getTreeList(self):
         return self.treeList
-    
-    def getCurrentPowerUp (self):
-        return self.currentPowerUp
 
     def setLives(self, lives):
         self.lives = lives
@@ -38,5 +35,4 @@ class Player():
     def setTreeList(self, treeList):
         self.treeList = treeList 
 
-    def setCurrentPowerUp (self, currentPowerUp):
-        self.currentPowerUp = currentPowerUp   
+
--- src/main/java/cr/ac/tec/PythonClient/GameFeatures/Token.py
+++ src/main/java/cr/ac/tec/PythonClient/GameFeatures/Token.py
@@ -1,3 +1,6 @@
+import json
+from collections import namedtuple
+from json import JSONEncoder
 class Token():
     def __init__(self, val, shape):
         super().__init__()
@@ -10,6 +13,25 @@ class Token():
     def getShape(self):
         return self.shape
 
+def tokenToJson(token):
+    string = json.dumps(token.__dict__)
+    data = {
+        "Token" : string
+    }
+    print(data)
+    return string
+
+def jsonToToken(tokenDict):
+    return namedtuple('X', tokenDict.keys())(*tokenDict.values())
+
+def parsedDict(dict , object_hook):
+    token = json.loads(dict, object_hook=object_hook)
+    return token
+
+token = Token(12, "Triangle")
 
 
+myToken = parsedDict(tokenToJson(token), jsonToToken)
+print(myToken.val)
+print(myToken.shape)
 
