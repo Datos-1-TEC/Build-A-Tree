4b90ceffe167be9ea0f15248a43b56ff13194324
Juan P. Rostran <54974335+juanprrr@users.noreply.github.com>
2020-12-08 01:14:13 -0600

    [Update] Clientside
    Se trasladaron los métodos de parsear tokens para
    agregarlos a un atributo de la clase client metidos en una lista.
    Se agregan los getters y setters de cada lista con los tokens de ese reto mandado por el server

--- src/main/java/cr/ac/tec/PythonClient/GameGui/GameClient.py
+++ src/main/java/cr/ac/tec/PythonClient/GameGui/GameClient.py
@@ -4,6 +4,8 @@ import json
 from threading import*
 from json import JSONEncoder
 from Player import*
+from Token import*
+from collections import namedtuple
 host = "127.0.0.1"
 port = 6666
 flag = True
@@ -11,9 +13,8 @@ format = "utf8"
 bts = 4096
 
 firstMessage = "Connected"
-
 class clientSide (Thread):
-    def __init__(self):
+    def __init__(self, player1, player2):
         
         self.port = port
         self.flag = True
@@ -22,6 +23,11 @@ class clientSide (Thread):
         self.request.connect((self.host, self.port))
         self.sendMessage(firstMessage)
         self.decoded = ""
+        self.player1 = player1 # Jugador uno creado en la GUI
+        self.player12 = player2 # Jugador dos creado en la GUI
+        self.mainTokens = [] #Lista de tokens principales del reto
+        self.fillerTokens = [] #Lista de tokens de relleno
+
         Thread.__init__(self)
 
         while (self.flag):
@@ -41,14 +47,13 @@ class clientSide (Thread):
 
     def processReceived(self, message):
         self.message = message
-        
         try:
             if "Challenges" in self.message:  
                 message_dict = json.loads(self.message)          
                 with open('JsonResources/CurrentChallenge.json', 'w') as json_file:
                     json.dump(message, json_file) 
-
                 print(json.dumps(message_dict))
+                self.readChallenge(self.mainTokens, self.fillerTokens)
                 
             elif self.message == "challenges":
                 print(self.message)
@@ -62,11 +67,93 @@ class clientSide (Thread):
                 print(self.message)    
         except IOError as e:
             print(e)
-              
+    
+    def getMainTokens(self):
+        return self.mainTokens
+    def getFillerTokens(self):
+        return self.fillerTokens
+
+    def setMainTokens(self, mainTokens):
+        self.mainTokens = mainTokens
+    def setFillerTokens(self, fillerTokens):
+        self.fillerTokens = fillerTokens
+
+    #Método que recibe un token y una id de jugador y que retorna un 
+    # mensaje formato json para mandarlo al server
+    def sendToken(self, Token, playerID):
+        data = {
+            str(playerID):{
+                "Token": {
+                    "shape": Token.getShape(),
+                    "value": Token.getVal()
+                }
+            }
+        }
+        with open('JsonResources/CurrentToken.json', 'w') as write_file:
+            json.dump(data, write_file)
+        message = json.dumps(data)
+        print(message)
+        return message
+    #Método que toma un diccionario con info de token para parsearlo a objeto de Python
+    def jsonToToken(self, tokenDict): 
+        return namedtuple('Token', tokenDict.keys())(*tokenDict.values())
+
+    #Esta función se encarga de leer el json donde están los tokens de ese reto
+    #estos tokens se agregan a las listas mainTokens y fillerTokens
+    def readChallenge(self, mainTokens, fillerTokens):
+        file_path = 'JsonResources/CurrentChallenge.json'
+        with open(file_path,'r') as read_file:
+            data = json.load(read_file)
+
+        data_dict = json.loads(data)
+        #mainTokens_dict
+        mainTokens_dict = data_dict["Challenges"]["MainTokens"]
+        fillerTokens_dict = data_dict["Challenges"]["FillerTokens"]
+        #Agregando main tokens
+        for key in mainTokens_dict:
+            token_dict = mainTokens_dict.get(key)
+            token_string = json.dumps(token_dict)
+            token_object = json.loads(token_string, object_hook=self.jsonToToken)
+            myFinalToken = Token(token_object.value, token_object.shape)
+            mainTokens.append(myFinalToken)
+        #Agregando filler tokens
+        for key in fillerTokens_dict:
+            token_dict = fillerTokens_dict.get(key)
+            token_string = json.dumps(token_dict)
+            token_object = json.loads(token_string, object_hook=self.jsonToToken)
+            myFinalToken = Token(token_object.value, token_object.shape)
+            fillerTokens.append(myFinalToken) 
+        for token in range(len(fillerTokens)):
+            print(fillerTokens[token].getShape())
+        
+        self.setMainTokens(mainTokens)
+        self.setFillerTokens(fillerTokens)
+
+def tokenToJson(token):
+    string = json.dumps(token.__dict__)
+    print("El token es: ")
+    print(string)
+    return string
+
+
+def parsedDict(dict , object_hook):
+    token = json.loads(dict, object_hook=object_hook)
+    return token
+    
+
+
+
+
+    
+
+
 def main():
-    c1 = clientSide()
+    player1 = Player(1)
+    player2 = Player(2)
+    c1 = clientSide(player1, player2)
     c1.start()
+    
 
 if __name__ == "__main__":
     main()
-print("Client closed....")
+print("Client closed....") 
--- src/main/java/cr/ac/tec/PythonClient/GameGui/Token.py
+++ src/main/java/cr/ac/tec/PythonClient/GameGui/Token.py
@@ -13,7 +13,6 @@ class Token():
         self.shape = shape
         self.points = 0
 
-
     def getVal(self):
         return self.val
     
@@ -26,73 +25,6 @@ class Token():
     def setPoints(self, points):
         self.points = points
 
-def tokenToJson(token):
-    string = json.dumps(token.__dict__)
-    print("El token es: ")
-    print(string)
-    return string
-
-def jsonToToken(tokenDict):
-    return namedtuple('Token', tokenDict.keys())(*tokenDict.values())
-
-def parsedDict(dict , object_hook):
-    token = json.loads(dict, object_hook=object_hook)
-    return token
-    
-def sendToken(Token, playerID):
-    jsonToken = json.dumps(Token.__dict__)
-    data = {
-        str(playerID):{
-            "Token": {
-                "shape": Token.getShape(),
-                "value": Token.getVal()
-            }
-        }
-    }
-    with open('JsonResources/CurrentToken.json', 'w') as write_file:
-        json.dump(data, write_file)
-    message = json.dumps(data)
-    print(message)
-    return message
-
-def readChallenge():
-    mainTokens = []
-    fillerTokens = []
-    file_path = 'JsonResources/CurrentChallenge.json'
-    with open(file_path,'r') as read_file:
-        data = json.load(read_file)
-
-    data_dict = json.loads(data)
-    #mainTokens_dict
-    print(data_dict["Challenges"]["MainTokens"])
-    print(data_dict["Challenges"]["FillerTokens"])
-    mainTokens_dict = data_dict["Challenges"]["MainTokens"]
-    fillerTokens_dict = data_dict["Challenges"]["FillerTokens"]
-
-    for key in mainTokens_dict:
-        token_dict = mainTokens_dict.get(key)
-        token_string = json.dumps(token_dict)
-        token_object = json.loads(token_string, object_hook=jsonToToken)
-        print(token_object.value)
-        myFinalToken = Token(token_object.value, token_object.shape)
-        mainTokens.append(myFinalToken)
-    print("Fin main tokens")
-
-    for key in fillerTokens_dict:
-        token_dict = fillerTokens_dict.get(key)
-        token_string = json.dumps(token_dict)
-        token_object = json.loads(token_string, object_hook=jsonToToken)
-        myFinalToken = Token(token_object.value, token_object.shape)
-        print(token_object.value)
-        fillerTokens.append(myFinalToken)
-    
-
-
-
-
-
-#print(x.name, x.hometown.name, x.hometown.id)
-readChallenge() 
 
 
 
